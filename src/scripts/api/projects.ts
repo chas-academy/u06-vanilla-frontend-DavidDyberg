d397b5431444e9582e6bd3d67f674e05729bdd57
David Dyberg <david.dyberg@gmail.com>
2025-04-04 18:00:29 +0200

    feat: add projects

--- src/scripts/api/projects.ts
+++ src/scripts/api/projects.ts
@@ -17,9 +17,44 @@ export async function fetchLimitedProjectData() {
 
     projectNameElements.forEach((element, index) => {
       if (data[index]) {
-        element.textContent = data[index].title; // Update with the project title
+        element.textContent = data[index].title;
       }
     });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+export async function fetchAllProjectData() {
+  const apiUrl = "https://david-dyberg-portfolio-api.vercel.app/api/projects";
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: projectType[] = await response.json();
+
+    const projectsContainer = document.querySelector(".projects-container");
+
+    if (projectsContainer) {
+      projectsContainer.innerHTML = "";
+
+      data.forEach((project) => {
+        const projectElement = document.createElement("div");
+        projectElement.classList.add("project-item");
+        projectElement.innerHTML = `
+          <h3 class="project-name">${project.title}</h3>
+          <img
+            class="project-image"
+            src="./assets/project-fake-image.png"
+            alt="Project image"
+          />
+        `;
+        projectsContainer.appendChild(projectElement);
+      });
+    }
   } catch (error) {
     console.error("Error fetching data:", error);
   }
